15c9ce2574a6a8c639e6094ac21f195816654661
R <merinogeospatial@gmail.com>
2018-09-22 14:21:44 -0400

    update serve.js to test

--- server.js
+++ server.js
@@ -30,6 +30,12 @@ dbConnect.once("open", function() {
   console.log("Mongoose connection successful.");
 });
 
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 //test route
 
 app.get("/", function(req, res) {
