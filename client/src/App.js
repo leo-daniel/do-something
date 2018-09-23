56428bb18d43dd0b75dfd473bdd70cbec6eb4e85
Scott Johnson <sjohnson.wvu@gmail.com>
2018-09-23 11:08:53 -0400

    Added Router to App.js with  /, register, and /user/:id

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import Main from './pages/Main';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+// Should add a 'no match' page later.
+// import NoMatch from "./pages/NoMatch";
 
-class App extends Component {
-  render() {
-    return <Fragment>
-      <Main />
-
-    </Fragment>
-      
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/user/:id" component={Profile} />
+        {/* <Route component={NoMatch} /> */}
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
