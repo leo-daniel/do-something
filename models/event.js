3981dbce2e4a79e3a49f8e9fce5e324729a442da
R <merinogeospatial@gmail.com>
2018-09-23 11:10:47 -0400

    transfer event-model branch to base off of develop branch instead of unmerged map branch

---
+++ models/event.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  eventName: { 
+    type: String,
+    trim: true,
+    required: [true, 'Event must have a name']
+  },
+  description: { 
+    type: String,
+    trim: true, 
+  },
+  organizerID: { 
+    type: Schema.Types.OnjectId,
+    ref: "User"
+  },
+  location: {
+    address: {
+      type: String,
+      trim: true
+    },
+    city: {
+      type: String,
+      trim: true
+    },
+    state: {
+      type: String,
+      trim: true
+    },
+    latitude: { 
+        type: String, 
+    },
+    longitude: {
+        type: String,
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  startTime: {
+      type: String,
+      trim: true
+  },
+  endTime: {
+      type: String,
+      trim: true
+  }
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+module.exports = Event;
