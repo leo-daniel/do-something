a5155f55ae56fd7bf33518f224eaa75a3f881f6a
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-09-26 22:48:41 -0400

    descructured component imports

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,7 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
+import { Button, Grid, Paper, TextField, Typography } from "@material-ui/core";
 
 const styles = theme => ({
   container: {
