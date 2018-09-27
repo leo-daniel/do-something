6f0ed7ae21c421bf7d49ca779607668a8cb66c0b
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-09-26 22:02:45 -0400

    adding event card component

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,54 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { url } from 'inspector';
+import React, { Fragment } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { PlaceTwoTone, CalendarTodayTwoTone } from "@material-ui/icons";
 
-const styles = {
+const styles = theme => ({
   card: {
-    maxWidth: 345,
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: 496
   },
-  media: {
-    // ⚠️ object-fit is not supported by IE11.
-    objectFit: 'cover',
+  cardContent: {
+    flexGrow: 1
   },
-};
+  icon: {
+    fontSize: "1rem",
+    marginRight: ".5rem"
+  }
+});
 
-function EventCard(props) {
-    const { classes } = props;
-    return (
-      <Card className={classes.card} style={{backgroundImage: url('https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=96e8c52b31b6e86b4abf5a20d9845793&auto=format&fit=crop&w=2250&q=80')}}>
-        <CardActionArea>
-          <CardContent>
-            <Typography gutterBottom variant="headline" component="h2">
-              Lizard
-            </Typography>
-            <Typography component="p">
-              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
+function Event(props) {
+  const { classes } = props;
+
+  return (
+    <Fragment>
+      <Card className={classes.card}>
+        <CardContent className={classes.cardContent}>
+          <Typography variant="headline" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="subheading" component="h3">
+            <PlaceTwoTone className={classes.icon} />
+            {props.city}, {props.state}
+          </Typography>
+          <Typography gutterBottom variant="subheading" component="h3">
+            <CalendarTodayTwoTone className={classes.icon} />
+            {props.date} at {props.time}
+          </Typography>
+          <Typography>{props.description}</Typography>
+        </CardContent>
         <CardActions>
           <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
+            <a href={`/${props.eventId}`}>Learn More</a>
           </Button>
         </CardActions>
       </Card>
-    );
-  }
-  
-  EventCard.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
-  export default withStyles(styles)(EventCard);
+    </Fragment>
+  );
+}
+
+export default withStyles(styles)(Event);
