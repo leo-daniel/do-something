0a630e5e06ffd568e50c00e61c5e0e127e2e3128
Scott Johnson <sjohnson.wvu@gmail.com>
2018-09-26 22:14:35 -0400

    Updated Login page elements

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,12 +1,50 @@
 import React from "react";
 import "./Hero.css";
-import Image from './clark-tibbs-367075-unsplash.jpg';
+import Image from "./clark-tibbs-367075-unsplash.jpg";
+import Paper from "@material-ui/core/Paper";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
+const styles = theme => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    },
+  }
+});
 
-const Hero = props => (
-  <div className="hero text-center" component="img" style={{ backgroundImage: `url(${Image})` }}>
-    {props.children}
-  </div>
-);
+class Hero extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+    <main className={classes.layout}>
+      <Paper className={classes.paper}
+      component="img"
+      style={{ backgroundImage: `url(${Image})` }}>
+        
+      </Paper>
+    </main>
+    );
+  }
+}
 
-export default Hero;
+Hero.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Hero);
---
+++ client/src/components/Login.js
@@ -0,0 +1,128 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginTop: 3,
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    marginBottom: 3
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    marginTop: theme.spacing.unit * 2,
+    marginLeft: theme.spacing.unit
+  },
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    }
+  }
+});
+
+class OutlinedTextFields extends React.Component {
+  state = {
+    // State information was copied in from material-ui example; will need to edit later or remove.
+    // email: 'john.doe@gmail.com',
+    // age: '',
+    // multiline: 'Controlled',
+    // currency: 'EUR',
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <form className={classes.container} noValidate autoComplete="off">
+            <Typography variant="title" gutterBottom>
+              User Registration
+            </Typography>
+            <Grid container justify="center" spacing={16}>
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-email-input"
+                  label="Email"
+                  className={classes.textField}
+                  type="email"
+                  name="email"
+                  autoComplete="email"
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-password-input"
+                  label="Password"
+                  className={classes.textField}
+                  type="password"
+                  autoComplete="current-password"
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.handleNext}
+                  className={classes.button}
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </main>
+    );
+  }
+}
+
+OutlinedTextFields.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(OutlinedTextFields);
