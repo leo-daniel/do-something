eff8eef010ccc5ee19b6f0e616246e2db28803dc
Leo Daniel <danielj1122@gmail.com>
2018-09-27 19:57:18 -0400

    Dependency updates; added connect-flash

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const passport = require("passport");
-const mongoStore = require("connect-mongo")(session);
-const dbConnection = require("./database");
-const routes = require("./routes");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const mongoStore = require('connect-mongo')(session);
+const dbConnection = require('./database');
+const routes = require('./routes');
+const cors = require('cors');
+const flash = require('connect-flash');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,18 +15,18 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Passport initialization & setup
 app.use(cors());
 app.use(
   session({
-    secret: "gregnate",
+    secret: 'gregnate',
     store: new mongoStore({ mongooseConnection: dbConnection }),
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
@@ -35,14 +36,13 @@ app.use(passport.session());
 // Add routes, both API and view
 app.use(routes);
 
-
-// Initialize Passport 
-require("./config/passport")(passport);
+// Initialize Passport
+require('./config/passport')(passport);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 // Start the API server
