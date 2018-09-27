6cf56fd98be6bb3c2708b8b1de34057324103a70
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-09-26 22:19:31 -0400

    added sample components

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
-import Navbar from '../components/Navbar'
-import Event from '../components/Event'
+import Navbar from '../components/Navbar';
+// import Event from '../components/Event'
+import Album from '../components/Album';
 
 export default props =>
     <div>
         <Navbar />
         {/* TODO: the Event component needs to be mapped to the events data */}
-        <Event />
+        {/* <Event /> */}
+        <Album />
     </div>
