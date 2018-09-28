2ce3bd77b27669dc57c09a67bbd0d0129ead3582
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-09-27 21:30:46 -0400

    added props fro EventCard

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,12 +1,33 @@
-import React, { Component, Fragment } from 'react';
-import Navbar from '../components/Navbar';
-// import Event from '../components/Event'
-import Album from '../components/Album';
+import React, { Component, Fragment } from "react";
+import API from "../utils/API";
+import Navbar from "../components/Navbar";
+import EventCard from "../components/EventCard";
+import Album from "../components/Album";
 
-export default props =>
-    <div>
+// stateful component
+class EventFeed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      EventCardData: null
+    };
+  }
+  componentWillMount() {
+    API.getEvents()
+      .then(res => {
+        console.log(res);
+        this.setState({ EventCardData: res });
+      })
+      .catch(error => console.log(error));
+  }
+  render() {
+    return (
+      <div>
         <Navbar />
-        {/* TODO: the Event component needs to be mapped to the events data */}
-        {/* <Event /> */}
-        <Album />
-    </div>
+        <EventCard data={this.state.EventCardData} />
+      </div>
+    );
+  }
+}
+
+export default EventFeed;
