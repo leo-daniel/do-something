1477b690dcc2fe73ba334e611be81467f95a0c57
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-09-28 21:03:33 -0400

    Events.js maps props to cards

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,32 +1,45 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import API from "../utils/API";
 import Navbar from "../components/Navbar";
 import EventCard from "../components/EventCard";
-import Album from "../components/Album";
+// import Album from "../components/Album";
 
 // stateful component
 class EventFeed extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      EventCardData: null
+      events: []
     };
   }
+  
   componentWillMount() {
     API.getEvents()
       .then(res => {
-        console.log(res);
-        this.setState({ EventCardData: res });
+        // console.log(JSON.stringify(res, undefined, 2));
+        this.setState({ events: res.data });
       })
       .catch(error => console.log(error));
   }
+
   render() {
     return (
       <div>
+        {console.log(JSON.stringify(this.state.events, undefined, 2))}
         <Navbar />
-        <EventCard data={this.state.EventCardData} />
+        {this.state.events.map((event) => 
+        <EventCard
+            key={event._id}
+            title={event.eventName}
+            city={event.city}
+            usstate={event.state}
+            // date={event.date} TODO: need to convert UTC to readable date
+            time={event.startTime}
+            description={event.description}
+            id={event._id}
+        />)}
       </div>
-    );
+    )
   }
 }
 
