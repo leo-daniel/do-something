9333bc3372e16bd631d345015bd4b64bfe0abfa9
R <merinogeospatial@gmail.com>
2018-09-28 21:56:23 -0400

    remove proxies...again

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require("./config/passport")(passport);
 
 // Serve up static assets (usually on heroku)
 // if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   // Send every request to the React app
   // Define any API routes before this runs
