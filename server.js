132465ec94c1d2567c03ea7e17f8d42797a55258
R <merinogeospatial@gmail.com>
2018-09-29 01:18:30 -0400

    update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require("./config/passport")(passport);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 
 // Send every request to the React app
