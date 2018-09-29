d3c5dad7d9f4a5264d56b8c9b450c68b0a2af243
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-09-28 21:03:01 -0400

    event card is set up correctly

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,59 +1,59 @@
-import React, { Fragment, Card, CardContent, CardActions, Button } from "react";
+import React, { Fragment } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography
+} from "@material-ui/core";
 import { PlaceTwoTone, CalendarTodayTwoTone } from "@material-ui/icons";
 
-// const styles = theme => ({
-//   card: {
-//     height: "100%",
-//     display: "flex",
-//     flexDirection: "column",
-//     maxWidth: 496
-//   },
-//   cardContent: {
-//     flexGrow: 1
-//   },
-//   icon: {
-//     fontSize: "1rem",
-//     marginRight: ".5rem"
-//   }
-// });
+const styles = theme => ({
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: 496
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  icon: {
+    fontSize: "1rem",
+    marginRight: ".5rem"
+  }
+});
 
-function EventCard() {
-  // data props passed from Events.js
-  // const { classes, data } = props;
+function EventCard(props) {
+  // props passed from Events.js
+  const { classes } = props;
 
   return (
     <Fragment>
-      <Card >
-      <CardContent>
-        sdkjghncslkfasflkasreisfxudkshfnsdhfklnsdjfcxlasfdcsmndfcx
-        nacralsfisafasemrcfhxhsadhfnxlasfacilsfuiasfliafaesilcfuxr
-        dfhgcnsfosifmhseoifhc
-      </CardContent>
-        {/* <CardContent className={classes.cardContent}>
+      <Card className={classes.card}>
+        <CardContent className={classes.cardContent}>
           <Typography variant="headline" component="h2">
             {props.title}
           </Typography>
           <Typography variant="subheading" component="h3">
             <PlaceTwoTone className={classes.icon} />
-            {props.city}, {props.state}
+            {props.city}, {props.usstate}
           </Typography>
           <Typography gutterBottom variant="subheading" component="h3">
             <CalendarTodayTwoTone className={classes.icon} />
-            {props.date} at {props.time}
+            {/* {props.date} at */}{props.time}
           </Typography>
           <Typography>{props.description}</Typography>
         </CardContent>
         <CardActions>
           <Button size="small" color="primary">
-            <a href={`/${props.eventId}`}>Learn More</a>
+            <a href={`events/${props.id}`}>Learn More</a>
           </Button>
-        </CardActions> */}
+        </CardActions>
       </Card>
     </Fragment>
   );
 }
 
-// export default withStyles(styles)(EventCard);
-export default EventCard;
+export default withStyles(styles)(EventCard);
