7bafb7abbc22952384b13c157b666a12c5b4e5f5
R <merinogeospatial@gmail.com>
2018-09-28 22:42:27 -0400

    fix casing...again

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
 
   // Send every request to the React app
+  
   // Define any API routes before this runs
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
