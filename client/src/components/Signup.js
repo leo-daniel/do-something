5a1fa40c34725057bc57eec16e213c7f5d70c162
Scott Johnson <sjohnson.wvu@gmail.com>
2018-10-19 10:45:01 -0400

    Added paperAbout and mainGrid classes

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -43,6 +43,20 @@ const styles = theme => ({
       marginBottom: theme.spacing.unit * 6,
       padding: theme.spacing.unit * 3
     }
+	},
+	paperAbout: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    },
+    backgroundColor: theme.palette.grey[200]
+  },
+  mainGrid: {
+    marginTop: theme.spacing.unit * 3
   }
 });
 
