6f5878b9a3580d5adc5a4c27e78cabb06e780866
Scott Johnson <sjohnson.wvu@gmail.com>
2018-10-19 20:07:26 -0400

    Removed footer backgroundColor

--- client/src/components/PageFooter.js
+++ client/src/components/PageFooter.js
@@ -20,7 +20,6 @@ const styles = theme => ({
 		},
 	},
 	footer: {
-		// backgroundColor: theme.palette.background.paper,
 		marginTop: theme.spacing.unit * 8,
 		padding: `${theme.spacing.unit * 6}px 0`,
 	},
